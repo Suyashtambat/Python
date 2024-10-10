9da152107cc219c8d70149923528ca189763c6af
Suyash Tambat <suyashtambat345@gmail.com>
2024-10-10 16:18:56 +0530

    Add files via upload

---
+++ if statements..py
@@ -0,0 +1,38 @@
+score = 0
+
+if score >= 90:
+    print("Grade A")
+elif score >= 80:
+    print("Grade B")
+elif score >= 70:
+    print("Grade C")
+elif score >= 60:
+    print("Grade D")
+else:
+    print("Grade E")
+
+temp = -15
+
+if temp >= 30:
+    print("It's too hot")
+elif 20<= temp <=30:
+    print("It's hot")
+elif 10<= temp <20:
+    print("It's cold")
+else :
+    print("It's too cold")
+
+
+num = 10
+
+if num % 2==0:
+    print("It's an even number")
+else:
+    print("It's an odd number")
+
+
+light_color = 'yellow'
+if light_color =='red':
+    print("go")
+elif light_color =='yellow':
+    print("Slow down")
